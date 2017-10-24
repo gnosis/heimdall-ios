9dba57ce3dd3459ec2cc66fa2a1766cb41f71c4e
Luis Reisewitz <reisewitz@me.com>
2017-10-24 19:02:09 +0200

    regenerate solidity with bivrost 0.0.3

--- Heimdall/Solidity/Generated Types/BytesX-Generated.swift
+++ Heimdall/Solidity/Generated Types/BytesX-Generated.swift
@@ -1,162 +1,162 @@
-    extension Solidity {
-        public final class Bytes1: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 1
-            }
-        }
-        public final class Bytes2: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 2
-            }
-        }
-        public final class Bytes3: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 3
-            }
-        }
-        public final class Bytes4: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 4
-            }
-        }
-        public final class Bytes5: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 5
-            }
-        }
-        public final class Bytes6: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 6
-            }
-        }
-        public final class Bytes7: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 7
-            }
-        }
-        public final class Bytes8: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 8
-            }
-        }
-        public final class Bytes9: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 9
-            }
-        }
-        public final class Bytes10: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 10
-            }
-        }
-        public final class Bytes11: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 11
-            }
-        }
-        public final class Bytes12: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 12
-            }
-        }
-        public final class Bytes13: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 13
-            }
-        }
-        public final class Bytes14: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 14
-            }
-        }
-        public final class Bytes15: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 15
-            }
-        }
-        public final class Bytes16: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 16
-            }
-        }
-        public final class Bytes17: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 17
-            }
-        }
-        public final class Bytes18: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 18
-            }
-        }
-        public final class Bytes19: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 19
-            }
-        }
-        public final class Bytes20: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 20
-            }
-        }
-        public final class Bytes21: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 21
-            }
-        }
-        public final class Bytes22: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 22
-            }
-        }
-        public final class Bytes23: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 23
-            }
-        }
-        public final class Bytes24: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 24
-            }
-        }
-        public final class Bytes25: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 25
-            }
-        }
-        public final class Bytes26: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 26
-            }
-        }
-        public final class Bytes27: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 27
-            }
-        }
-        public final class Bytes28: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 28
-            }
-        }
-        public final class Bytes29: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 29
-            }
-        }
-        public final class Bytes30: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 30
-            }
-        }
-        public final class Bytes31: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 31
-            }
-        }
-        public final class Bytes32: _DoNotUse._BytesX {
-            override class var length: UInt {
-                return 32
-            }
+extension Solidity {
+    final class Bytes1: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 1
         }
     }
+    final class Bytes2: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 2
+        }
+    }
+    final class Bytes3: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 3
+        }
+    }
+    final class Bytes4: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 4
+        }
+    }
+    final class Bytes5: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 5
+        }
+    }
+    final class Bytes6: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 6
+        }
+    }
+    final class Bytes7: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 7
+        }
+    }
+    final class Bytes8: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 8
+        }
+    }
+    final class Bytes9: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 9
+        }
+    }
+    final class Bytes10: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 10
+        }
+    }
+    final class Bytes11: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 11
+        }
+    }
+    final class Bytes12: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 12
+        }
+    }
+    final class Bytes13: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 13
+        }
+    }
+    final class Bytes14: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 14
+        }
+    }
+    final class Bytes15: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 15
+        }
+    }
+    final class Bytes16: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 16
+        }
+    }
+    final class Bytes17: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 17
+        }
+    }
+    final class Bytes18: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 18
+        }
+    }
+    final class Bytes19: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 19
+        }
+    }
+    final class Bytes20: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 20
+        }
+    }
+    final class Bytes21: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 21
+        }
+    }
+    final class Bytes22: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 22
+        }
+    }
+    final class Bytes23: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 23
+        }
+    }
+    final class Bytes24: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 24
+        }
+    }
+    final class Bytes25: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 25
+        }
+    }
+    final class Bytes26: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 26
+        }
+    }
+    final class Bytes27: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 27
+        }
+    }
+    final class Bytes28: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 28
+        }
+    }
+    final class Bytes29: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 29
+        }
+    }
+    final class Bytes30: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 30
+        }
+    }
+    final class Bytes31: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 31
+        }
+    }
+    final class Bytes32: _DoNotUse._BytesX {
+        override class var length: UInt {
+            return 32
+        }
+    }
+}
--- Heimdall/Solidity/Generated Types/UIntX-Generated.swift
+++ Heimdall/Solidity/Generated Types/UIntX-Generated.swift
@@ -1,162 +1,162 @@
-    extension Solidity {
-        public final class UInt8: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 8
-            }
-        }
-        public final class UInt16: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 16
-            }
-        }
-        public final class UInt24: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 24
-            }
-        }
-        public final class UInt32: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 32
-            }
-        }
-        public final class UInt40: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 40
-            }
-        }
-        public final class UInt48: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 48
-            }
-        }
-        public final class UInt56: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 56
-            }
-        }
-        public final class UInt64: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 64
-            }
-        }
-        public final class UInt72: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 72
-            }
-        }
-        public final class UInt80: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 80
-            }
-        }
-        public final class UInt88: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 88
-            }
-        }
-        public final class UInt96: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 96
-            }
-        }
-        public final class UInt104: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 104
-            }
-        }
-        public final class UInt112: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 112
-            }
-        }
-        public final class UInt120: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 120
-            }
-        }
-        public final class UInt128: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 128
-            }
-        }
-        public final class UInt136: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 136
-            }
-        }
-        public final class UInt144: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 144
-            }
-        }
-        public final class UInt152: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 152
-            }
-        }
-        public final class UInt160: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 160
-            }
-        }
-        public final class UInt168: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 168
-            }
-        }
-        public final class UInt176: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 176
-            }
-        }
-        public final class UInt184: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 184
-            }
-        }
-        public final class UInt192: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 192
-            }
-        }
-        public final class UInt200: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 200
-            }
-        }
-        public final class UInt208: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 208
-            }
-        }
-        public final class UInt216: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 216
-            }
-        }
-        public final class UInt224: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 224
-            }
-        }
-        public final class UInt232: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 232
-            }
-        }
-        public final class UInt240: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 240
-            }
-        }
-        public final class UInt248: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 248
-            }
-        }
-        public final class UInt256: _DoNotUse._UIntX {
-            override class var bitWidth: UInt {
-                return 256
-            }
+extension Solidity {
+    final class UInt8: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 8
         }
     }
+    final class UInt16: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 16
+        }
+    }
+    final class UInt24: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 24
+        }
+    }
+    final class UInt32: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 32
+        }
+    }
+    final class UInt40: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 40
+        }
+    }
+    final class UInt48: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 48
+        }
+    }
+    final class UInt56: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 56
+        }
+    }
+    final class UInt64: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 64
+        }
+    }
+    final class UInt72: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 72
+        }
+    }
+    final class UInt80: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 80
+        }
+    }
+    final class UInt88: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 88
+        }
+    }
+    final class UInt96: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 96
+        }
+    }
+    final class UInt104: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 104
+        }
+    }
+    final class UInt112: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 112
+        }
+    }
+    final class UInt120: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 120
+        }
+    }
+    final class UInt128: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 128
+        }
+    }
+    final class UInt136: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 136
+        }
+    }
+    final class UInt144: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 144
+        }
+    }
+    final class UInt152: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 152
+        }
+    }
+    final class UInt160: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 160
+        }
+    }
+    final class UInt168: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 168
+        }
+    }
+    final class UInt176: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 176
+        }
+    }
+    final class UInt184: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 184
+        }
+    }
+    final class UInt192: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 192
+        }
+    }
+    final class UInt200: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 200
+        }
+    }
+    final class UInt208: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 208
+        }
+    }
+    final class UInt216: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 216
+        }
+    }
+    final class UInt224: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 224
+        }
+    }
+    final class UInt232: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 232
+        }
+    }
+    final class UInt240: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 240
+        }
+    }
+    final class UInt248: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 248
+        }
+    }
+    final class UInt256: _DoNotUse._UIntX {
+        override class var bitWidth: UInt {
+            return 256
+        }
+    }
+}
