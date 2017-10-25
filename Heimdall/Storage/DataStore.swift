4fb376e4bed89a3157530d41c11210306326e989
Luis Reisewitz <reisewitz@me.com>
2017-10-25 15:26:46 +0200

    Add DataStore protocol & implementations

---
+++ Heimdall/Storage/DataStore.swift
@@ -0,0 +1,36 @@
+//
+//  DataStore.swift
+//  Heimdall
+//
+//  Created by Luis Reisewitz on 25.10.17.
+//  Copyright © 2017 Gnosis. All rights reserved.
+//
+
+import Foundation
+
+protocol DataStore {
+    /// Stores the given object in the store. Can be retrieved later with the
+    /// same key.
+    ///
+    /// - Parameters:
+    ///   - data: The object to store.
+    ///   - key: The key to use for storing & retrieving.
+    /// - Throws: When storing fails.
+    func store<T>(_ data: T, for key: String) throws where T: Codable
+
+    /// Tries to fetch the object associated with that key.
+    ///
+    /// - Parameter key:  key to use for storing & retrieving.
+    /// - Returns: The object associated with that key.
+    /// - Throws: When fetching fails or no object is associated with that key.
+    func fetch<T>(key: String) throws -> T where T: Codable
+}
+
+/// Describes errors that can occur when storing & fetching objects.
+///
+/// - noSuchEntry: Key is not associated with any object.
+/// - invalidKey: Key contains illegal characters.
+enum DataStoreError: Error {
+    case noSuchEntry
+    case invalidKey
+}
---
+++ Heimdall/Storage/DiskDataStore.swift
@@ -0,0 +1,55 @@
+//
+//  DiskDataStore.swift
+//  Heimdall
+//
+//  Created by Luis Reisewitz on 25.10.17.
+//  Copyright © 2017 Gnosis. All rights reserved.
+//
+
+import Foundation
+import Disk
+
+/// Base class for disk data stores. Not meant to be used directly, only for
+/// subclassing.
+class DiskDataStore {
+    var directory: Disk.Directory {
+        die("DiskDataStore.directory needs to be overridden")
+    }
+
+    private func fileName(from key: String) throws -> String {
+        guard let escapedKey = key.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            throw DataStoreError.invalidKey
+        }
+        return "\(escapedKey).json"
+    }
+}
+
+// MARK: - DataStore
+extension DiskDataStore: DataStore {
+    func store<T>(_ data: T, for key: String) throws where T: Codable {
+        let filename = try fileName(from: key)
+        try Disk.save(data, to: directory, as: filename)
+    }
+
+    func fetch<T>(key: String) throws -> T where T: Codable {
+        let filename = try fileName(from: key)
+        return try Disk.retrieve(filename, from: directory, as: T.self)
+    }
+}
+
+/// Stores data in the ApplicationSupport directory.
+/// Should be used for data that is important but should not be visible to the user.
+/// Use `DocumentsDataStore` for user generated data.
+final class ApplicationSupportDataStore: DiskDataStore {
+    override var directory: Disk.Directory {
+        return .applicationSupport
+    }
+}
+
+/// Stores data in the Documents directory.
+/// Should be used for data that is important and should be visible to the user.
+final class DocumentsDataStore: DiskDataStore {
+    override var directory: Disk.Directory {
+        return .documents
+    }
+}
