bbddd060cb07b14783a57e1aa9c165f9bff9bd4a
Luis Reisewitz <reisewitz@me.com>
2017-11-02 14:02:27 +0100

    Test EtherRPC

--- HeimdallTests/EtherRPCSpec.swift
+++ HeimdallTests/EtherRPCSpec.swift
@@ -20,29 +20,39 @@ class EtherRPCSpec: QuickSpec {
                     return 1
                 }
             }
-            it("should call the rpc provider correctly & decode results") {
 
-                class MockProvider: JsonRpcProvider {
-                    let mockReturnValue = BigUInt(123456)
-                    var actualTransaction: Transaction?
+            class MockProvider: JsonRpcProvider {
+                let mockReturnUInt = BigUInt(123_456)
+                let mockReturnHash = Hash(hexString: "0x7238f257c2652c72e17fee12e6ee22d15d1ae87540e74dd73c318183e8f37739")
 
-                    override var chainId: ChainId {
-                        return .ChainIdKovan
-                    }
+                var actualTransaction: Transaction?
+                var actualSignedTransaction: Data?
 
-                    override func call(_ transaction: Transaction) -> DataPromise {
-                        actualTransaction = transaction
-
-                        let data = Data(fromHexEncodedString: (try! Solidity.UInt256(mockReturnValue)).encode())!
-                        let promise = DataPromise.resolved(data as NSData)!
-                        return promise
-                    }
+                override var chainId: ChainId {
+                    return .ChainIdKovan
                 }
 
+                override func call(_ transaction: Transaction) -> DataPromise {
+                    actualTransaction = transaction
+
+                    let data = Data(fromHexEncodedString: (try! Solidity.UInt256(mockReturnUInt)).encode())!
+                    let promise = DataPromise.resolved(data as NSData)!
+                    return promise
+                }
+
+                override func sendTransaction(_ signedTransaction: Data) -> HashPromise {
+                    actualSignedTransaction = signedTransaction
+
+                    return HashPromise.resolved(mockReturnHash)
+                }
+            }
+
+            it("should call function & decode results correctly") {
                 let credentials = try! Credentials(from: "legal winner thank year wave sausage worth useful legal winner thank yellow")
                 let fromAddress = Address(string: credentials.address)
-                let contractAddress = "f"
+                let contractAddress = "0x72E7780A3B3927d5784843ce1e58c1593bBaaF49"
                 let balanceAddress = try! Solidity.Address("0")
+                let functionCallData = String(StandardToken.BalanceOf.encodeCall(arguments: balanceAddress).dropFirst(2))
 
                 let provider = MockProvider()
                 let nonceProvider = MockNonceProvider()
@@ -50,16 +60,48 @@ class EtherRPCSpec: QuickSpec {
                 let rpc = EtherRPC(provider: provider, credentials: credentials, nonceProvider: nonceProvider)
 
                 waitUntil { done in
-                    rpc.call(StandardToken.BalanceOf.self,
-                             ofContractAt: contractAddress,
-                             with: balanceAddress)
+                    _ = rpc.call(StandardToken.BalanceOf.self,
+                                 ofContractAt: contractAddress,
+                                 with: balanceAddress)
                         .observeNext { returnUInt in
-                            expect(returnUInt.value) == provider.mockReturnValue
-                            expect(provider.actualTransaction) != nil
+                            expect(returnUInt.value) == provider.mockReturnUInt
+                            expect(provider.actualTransaction).toNot(beNil())
                             expect(provider.actualTransaction?.fromAddress) == fromAddress
                             expect(provider.actualTransaction?.chainId) == provider.chainId
                             expect(provider.actualTransaction?.nonce) == nonceProvider.nextNonce()
                             expect(provider.actualTransaction?.toAddress) == Address(string: contractAddress)
+                            expect(provider.actualTransaction?.data) == Data(fromHexEncodedString: functionCallData)!
+
+                            done()
+                    }
+                }
+            }
+
+            it("should sign & broadcast transactions correctly") {
+                let credentials = try! Credentials(from: "legal winner thank year wave sausage worth useful legal winner thank yellow")
+                let fromAddress = Address(string: credentials.address)
+                let contractAddress = "0x72E7780A3B3927d5784843ce1e58c1593bBaaF49"
+                let balanceAddress = try! Solidity.Address("0")
+                let functionCallData = String(StandardToken.BalanceOf.encodeCall(arguments: balanceAddress).dropFirst(2))
+
+                let provider = MockProvider()
+                let nonceProvider = MockNonceProvider()
+
+                let rpc = EtherRPC(provider: provider, credentials: credentials, nonceProvider: nonceProvider)
+
+                waitUntil { done in
+                    _ = rpc.sendTransaction(for: StandardToken.BalanceOf.self,
+                                            ofContractAt: contractAddress,
+                                            with: balanceAddress)
+                        .observeNext { hash in
+                            expect(hash) == provider.mockReturnHash
+                            let actualTransaction = Transaction(data: provider.actualSignedTransaction!)
+                            expect(actualTransaction).toNot(beNil())
+                            expect(actualTransaction.fromAddress) == fromAddress
+                            expect(actualTransaction.chainId) == provider.chainId
+                            expect(actualTransaction.nonce) == nonceProvider.nextNonce()
+                            expect(actualTransaction.toAddress) == Address(string: contractAddress)
+                            expect(actualTransaction.data) == Data(fromHexEncodedString: functionCallData)!
 
                             done()
                     }
